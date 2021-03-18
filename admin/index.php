55af6d3b2dcaa9434b2facfebe1f1af3174f3dce
unknown <pierreemad93@outlook.com>
2021-03-18 17:42:55 +0200

    init commit

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
-<?php  
- echo "hello world" ;
-?>
+<?php require "resources/includes/header.inc" ?>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary">click</button>
+   
+    <i class="fas fa-users"></i>
+    
+
+    
+
+<?php require "resources/includes/footer.inc" ?>
