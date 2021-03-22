2550174dbcdcd2850294dcb76b188459ef31d54b
pierreemad93 <pierreemad93@outlook.com>
2021-03-22 15:10:08 +0200

    init commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,15 +1,5 @@
 <?php
 session_start();
-// Set Language variable
-$_SESSION['lang']=isset($_GET['lang'])?$_GET['lang']:"en";
-// Include Language file
-if ($_SESSION['lang'] == "en") {
-    include "resources/lang/en.php";
-} elseif ($_SESSION['lang'] == "ar") {
-    include "resources/lang/ar.php";
-} else {
-    include "resources/lang/en.php";
-}
 ?>
 <?php require "config.php"?>
 <?php require "resources/includes/header.inc" ?>
